4a49770f227e43eb0ca04a15a0d9d187bbd1665d
Jennyf19 <ferriesj@gmail.com>
2016-07-15 09:59:03 -0700

    use radio button and images

---
+++ RadioButton/defaul.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="defaul.aspx.cs" Inherits="RadioButton.defaul" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="titleLabel" runat="server" Text="Your note taking preferences:"></asp:Label>
+        <br />
+        <br />
+        <asp:RadioButton ID="pencilButton" runat="server" OnCheckedChanged="pencilButton_CheckedChanged" Text="Pencil" GroupName="device" />
+        <br />
+        <asp:RadioButton ID="penButton" runat="server" OnCheckedChanged="penButton_CheckedChanged" Text="Pen" GroupName="device" />
+        <br />
+        <asp:RadioButton ID="phoneButton" runat="server" OnCheckedChanged="phoneButton_CheckedChanged" Text="Phone" GroupName="device" />
+        <br />
+        <asp:RadioButton ID="tabletButton" runat="server" OnCheckedChanged="tabletButton_CheckedChanged" Text="Tablet" GroupName="device" />
+        <br />
+        <asp:Button ID="okButton" runat="server" BackColor="#CC66FF" BorderStyle="Double" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    <p>
+        &nbsp;</p>
+        <p>
+            <asp:Image ID="resultImage" runat="server" />
+        </p>
+    </form>
+    </body>
+</html>
---
+++ RadioButton/defaul.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RadioButton
+{
+    public partial class defaul : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            if (pencilButton.Checked)
+            {
+                resultLabel.Text = "You selected pencil!";
+                resultImage.ImageUrl = "pencil.jpg";
+            }
+            else if (penButton.Checked)
+            {
+                resultLabel.Text = "You selected a pen!";
+                resultImage.ImageUrl = "pen.jpg";
+            }
+            else if (phoneButton.Checked)
+            {
+                resultLabel.Text = "you selected phone!";
+                resultImage.ImageUrl = "phone.jpg";
+            }
+            else if (tabletButton.Checked)
+            {
+                resultLabel.Text = "you selected tablet!";
+                resultImage.ImageUrl = "tablet.jpg";
+            }
+            else
+            {
+                resultLabel.Text = "Please select one of the above answers.";
+            }
+        }
+
+        protected void pencilButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void penButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void phoneButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void tabletButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
